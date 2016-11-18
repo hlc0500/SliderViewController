3528b792270ddd8298054086225f07674c545331
hlc0500/hlc89 <3036305192>
2016-11-18 14:22:28 +0800

    Initial Commit

---
+++ SliderViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SliderViewController
+//
+//  Created by hlc on 16/11/18.
+//  Copyright © 2016年 hlc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SliderViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SliderViewController
+//
+//  Created by hlc on 16/11/18.
+//  Copyright © 2016年 hlc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
